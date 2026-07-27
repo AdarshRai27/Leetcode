c4a2cf4b2650c93e1809fe012dc53c3994dad7ac
AdarshRai27 <125826479+AdarshRai27@users.noreply.github.com>
2026-07-27 11:23:23 +0530

    Time: 0 ms (100%), Space: 12.2 MB (99.33%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
-    long long int toint(string bin) {
-    return stoll(bin,nullptr,2);
-}
-    string tobin(int num,int size){
-    if(num==0)
-        return string(size,'0');
-    string binary;
-    while(num>0){
-        binary.push_back((num % 2)+'0');
-        num/=2;
+    char compliment(char x){
+        if(x=='1')return '0';
+        else return '1';
     }
-    reverse(binary.begin(),binary.end());
-    if(binary.length()<size)
-        binary=string(size-binary.length(),'0')+binary;
-    return binary;
-}
     string findDifferentBinaryString(vector<string>& nums) {
-        vector<long long int>arr;
-        if(nums[0].size()==1){
-            if(nums[0]=="1")return "0";
-            else return "1";
+        string ans="";
+        for(int i=0;i<nums.size();i++){
+            ans+=compliment(nums[i][i]);
         }
-        for(auto x:nums){
-            arr.push_back(toint(x));
-        }
-        sort(arr.begin(),arr.end());
-        long long start=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]!=start){
-                return tobin(start,nums[0].size());
-            }
-            else start++;
-        }
-        return tobin(start,nums[0].size());
+        return ans;
     }
 };
